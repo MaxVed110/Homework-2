a6e1da0d37f57291e9910d6bdccefa83a285a556
Maxim <vedernikovmaxim@rambler.ru>
2022-02-28 23:16:30 +0300

    Создан второй вариант программы

---
+++ Зад_10_Вторая_цифра_рандом/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = new int();
+int TwoNumber = new int();
+
+number = new Random().Next(100, 1000);
+TwoNumber = (number / 10) % 10;
+
+
+Console.Write("Рандомное трёхзначное число: ");
+Console.WriteLine(number.ToString());
+Console.WriteLine($"Вторая цифра числа - {TwoNumber}");
