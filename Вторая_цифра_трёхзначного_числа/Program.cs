0d93c6d18de5ad5bccfeebf801e3190906ac6455
Maxim <vedernikovmaxim@rambler.ru>
2022-02-28 23:09:08 +0300

    Создан первый вариант программы

---
+++ Вторая_цифра_трёхзначного_числа/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new int();
+int TwoNumber = new int();
+
+Console.Write("Введите трёхзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+TwoNumber = (number / 10) % 10;
+
+Console.WriteLine($"Вторая цифра числа - {TwoNumber}");
