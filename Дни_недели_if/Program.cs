22a687aa8038299d0ecd9cacf322ffb43cd27e41
Maxim <vedernikovmaxim@rambler.ru>
2022-03-01 22:45:15 +0300

    Создана вторая версия программы через if

---
+++ Дни_недели_if/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = new int();
+
+Console.Write("Введите номер дня недели: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 5)
+{
+    Console.Write("Не является выходным днём");
+}
+else
+{
+    if (number == 6 || number == 7)
+    {
+        Console.Write("Является выходным днём");
+    }
+    else
+    {
+        Console.Write("Такого дня недели не существует");
+    }
+}
+
