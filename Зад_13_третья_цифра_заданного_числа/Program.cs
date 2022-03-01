fc96b6dc3d84f67ec53a3ec303ddf8ff20ed69ce
Maxim <vedernikovmaxim@rambler.ru>
2022-03-01 22:21:25 +0300

    Создана программа поиска третьего числа

---
+++ Зад_13_третья_цифра_заданного_числа/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = new int();
+int numeral = new int();
+int count = new int();
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+count = 0;
+numeral = number;
+while (numeral > 0)
+{
+    numeral = numeral / 10;
+    count++;
+}
+if (count >= 3)
+{
+    number = number / Convert.ToInt32(Math.Pow(10, (count-3)));
+    number = number % 10;
+    Console.WriteLine($"Третья цифра числа - {number}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
